2704785aef19382953ae9ddb79b0a2905b862439
godls4222 <72494486+godls4222@users.noreply.github.com>
2020-11-01 14:00:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* main.js */ 
+
+// 2단계 메뉴
+$(function(){
+    $('.depth1 > li').hover(
+        function(){
+            $('.depth2').stop().slideDown(200);
+        },
+        function(){
+            $('.depth2').stop().slideUp(200);
+        }
+    );
+})
+
+// bxslider
+$(document).ready(function(){
+
+    $('#main_visual').imagesLoaded( function() {
+        // images have loaded
+        $('.slider').bxSlider({
+            // 효과: horizontal , vertical, fade
+            mode:'horizontal',
+            // 인터벌 시간
+            pause: 4000,
+            // 자동재생: true | false
+            auto:true
+        });
+    });
+});
